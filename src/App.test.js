2965c5edbbd4e58d56b13f4bcedcf69f2ca3ead2
Leticia <lettycuevas91@gmail.com>
2023-01-14 11:11:05 -0300

    update increment

--- src/App.test.js
+++ src/App.test.js
@@ -16,8 +16,4 @@ it('renders the button', () => {
   expect(wrapper.find('button#inc').length).toBe(1);
 });
 
-it('increments the value', () => {
-  const wrapper = shallow(<App />);
-  wrapper.find('button').simulate('click');
-  expect(wrapper.find('.value').length).toBe(1);
-});
+
